349f0397341d3efe17ded47938d2c5a154718262
helman101 <helmanandresortegon@gmail.com>
2021-10-23 23:47:29 +0200

    create function to create student

---
+++ main.rb
@@ -0,0 +1,26 @@
+require_relative './example_school_library_decorator/book'
+require_relative './example_school_library_decorator/person'
+require_relative './example_school_library_decorator/student'
+require_relative './example_school_library_decorator/teacher'
+require_relative './example_school_library_decorator/classroom'
+require_relative './example_school_library_decorator/rental'
+
+def create_student
+  print "Age: "
+  age = gets.chomp
+  print "Name: "
+  name = gets.chomp
+  print "Has parent permission? [Y/N]: "
+  permission = gets.chomp
+  puts
+  if permission == 'y' || permission == 'Y'
+    permission = true
+  elsif permission == 'n' || permission == 'N'
+    permission = false
+  else
+    puts 'Invalid input, try again'
+    puts
+    create_student()
+  end
+  Student.new(age, Classroom.new("Comics"), name, permission)
+end
