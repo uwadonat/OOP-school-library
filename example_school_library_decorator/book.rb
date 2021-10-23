59e3cd4dfd670eb7fae299fb61a534f2df8eb2ea
helman101 <helmanandresortegon@gmail.com>
2021-10-23 23:45:40 +0200

    create class instance to hold all the books

--- example_school_library_decorator/book.rb
+++ example_school_library_decorator/book.rb
@@ -2,11 +2,14 @@ class Book
   attr_accessor :author, :title, :rentals
   attr_reader :id
 
+  @@book_list = []
+
   def initialize(author, title)
-    @id = random.rand(1..1000)
+    @id = Random.rand(1..1000)
     @rentals = []
     @title = title
     @author = author
+    @@book_list << self
   end
 
   def add_rental(person, date)
